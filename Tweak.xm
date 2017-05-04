f1c6560d9ba14f009e760db8bb48e5e2742cdf1c
Dani Winter <mail@daniwinter.nl>
2017-05-04 15:53:53 +0200

    Fixed bug where settings would not save

--- Tweak.xm
+++ Tweak.xm
@@ -1,14 +1,28 @@
-NSDateFormatter *format = [[NSDateFormatter alloc] init];
-NSDate *date = [NSDate date];
 NSString *Format = @"MM-dd-yyyy";
 NSString *timeLocation = @"1";
 BOOL Enabled = YES;
 BOOL timeEnabled = NO;
+BOOL debugging = NO;
 
 %hook SBStatusBarStateAggregator
 -(void)_updateTimeItems {
+
+  if (debugging) {
+    NSLog(@"StatusbarDate: ENABLED: %d", Enabled);
+  }
+
+  NSDateFormatter *format = [[NSDateFormatter alloc] init];
+
   if (Enabled) {
+    if (debugging) {
+      NSLog(@"StatusbarDate: TIME ENABLED: %d", timeEnabled);
+    }
+
     if (timeEnabled) {
+      if (debugging) {
+        NSLog(@"StatusbarDate: TIME LOCATION: %@", timeLocation);
+      }
+
       if ([timeLocation isEqualToString:@"1"]) {
         [format setDateFormat:[NSString stringWithFormat:@"HH:mm - %@", Format]];
       }
@@ -19,15 +33,24 @@ BOOL timeEnabled = NO;
     else {
       [format setDateFormat:[NSString stringWithFormat:@"%@", Format]];
     }
+
+    if (debugging) {
+      NSLog(@"StatusbarDate: UPDATE TIME");
+    }
+
     MSHookIvar<NSDateFormatter *>(self, "_timeItemDateFormatter") = format;
-    date = nil;
   }
   %orig;
 }
 %end
 
 static void loadPrefs() {
+  if (debugging) {
+    NSLog(@"StatusbarDate: Loading preferences..");
+  }
+
   NSMutableDictionary *prefs = [[NSMutableDictionary alloc] initWithContentsOfFile:@"/var/mobile/Library/Preferences/nl.d4ni.statusbardateprefs.plist"];
+
   if(prefs) {
     Enabled = ( [prefs objectForKey:@"kEnabled"] ? [[prefs objectForKey:@"kEnabled"] boolValue] : Enabled );
     timeEnabled = ( [prefs objectForKey:@"timeEnabled"] ? [[prefs objectForKey:@"timeEnabled"] boolValue] : timeEnabled );
@@ -38,6 +61,6 @@ static void loadPrefs() {
 }
 
 %ctor {
-  CFNotificationCenterAddObserver(CFNotificationCenterGetDarwinNotifyCenter(), NULL, (CFNotificationCallback)loadPrefs, CFSTR("nl.d4ni.statusbardateprefs/settingschanged"), NULL, CFNotificationSuspensionBehaviorCoalesce);
+  CFNotificationCenterAddObserver(CFNotificationCenterGetDarwinNotifyCenter(), NULL, (CFNotificationCallback)loadPrefs, CFSTR("nl.d4ni.plsnoresetprefs/settingschanged"), NULL, CFNotificationSuspensionBehaviorCoalesce);
   loadPrefs();
 }
--- statusbardateprefs/StatusbarDatePrefs.mm
+++ statusbardateprefs/StatusbarDatePrefs.mm
@@ -7,6 +7,24 @@
 @end
 
 @implementation StatusbarDatePrefsListController
+
+- (id)readPreferenceValue:(PSSpecifier*)specifier {
+	NSString *path = [NSString stringWithFormat:@"/User/Library/Preferences/%@.plist", specifier.properties[@"defaults"]];
+	NSDictionary *settings = [NSDictionary dictionaryWithContentsOfFile:path];
+	return (settings[specifier.properties[@"key"]]) ?: specifier.properties[@"default"];
+}
+
+- (void)setPreferenceValue:(id)value specifier:(PSSpecifier*)specifier {
+	NSString *path = [NSString stringWithFormat:@"/User/Library/Preferences/%@.plist", specifier.properties[@"defaults"]];
+	NSMutableDictionary *settings = [NSMutableDictionary dictionaryWithContentsOfFile:path];
+	[settings setObject:value forKey:specifier.properties[@"key"]];
+	[settings writeToFile:path atomically:YES];
+	CFStringRef notificationName = (CFStringRef)specifier.properties[@"PostNotification"];
+	if (notificationName) {
+		CFNotificationCenterPostNotification(CFNotificationCenterGetDarwinNotifyCenter(), notificationName, NULL, NULL, YES);
+	}
+}
+
 - (id)specifiers {
 	if(_specifiers == nil) {
 		_specifiers = [[self loadSpecifiersFromPlistName:@"StatusbarDatePrefs" target:self] retain];
@@ -15,11 +33,29 @@
 }
 
 -(void)save {
-	pid_t pid;
-	int status;
-	const char *argv2[] = {"killall", "SpringBoard", NULL};
-	posix_spawn(&pid, "/usr/bin/killall", NULL, NULL, (char* const*)argv2, NULL);
-	waitpid(pid, &status, WEXITED);
+	// [self showMessage:@"Device will respring in 5 seconds."
+  //                   withTitle:@"Saving.."];
+	// double delayInSeconds = 7.0;
+	// dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
+	// dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+		pid_t pid;
+		int status;
+		const char *argv2[] = {"killall", "SpringBoard", NULL};
+		posix_spawn(&pid, "/usr/bin/killall", NULL, NULL, (char* const*)argv2, NULL);
+		waitpid(pid, &status, WEXITED);
+	// });
+}
+
+-(void)showMessage:(NSString*)message withTitle:(NSString *)title{
+	dispatch_async(dispatch_get_main_queue(), ^{
+	    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:title message:message preferredStyle:UIAlertControllerStyleAlert];
+	    // [alertController addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+			//
+	    // }]];
+
+	    [[[[UIApplication sharedApplication] keyWindow] rootViewController] presentViewController:alertController animated:YES completion:^{
+	    }];
+	});
 }
 
 - (void)donate {
